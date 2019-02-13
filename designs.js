83bb5293b48bf3db0019122f9a9af5ced9f20d79
howdyhooers <ashley@benne.net>
2019-02-12 21:21:21 -0600

    Improve code comments, correct inverted width and height

--- designs.js
+++ designs.js
@@ -1,36 +1,38 @@
-// Select color input
-// Select size input
+/**
+ *The table element is initially empty. And will be populated with cells
+ *according to user input via makeGrid function.
+ *columnInput will be the width input by user.
+ *rowInput will be the height input by user.
+ *colorPicker allows user to choose a color to paint each cell.
+ */
+const table = document.getElementById('pixelCanvas');
+const columnInput = document.getElementById('inputWidth');
+const rowInput = document.getElementById('inputHeight');
+const colorPicker = document.getElementById('colorPicker');
 
-// When size is submitted by the user, call makeGrid()
-
+/**
+ * @description The purpose of this function is to create
+ *a grid of cells in response to a form submission, which contains
+ *input of height and width from user.  When user clicks on a cell
+ *the color should change according to colorPicker choice.
+ */
 function makeGrid() {
-
-  // get input from DOM
-  const table = document.getElementById('pixelCanvas');
-  const rowInput = document.getElementById('inputWidth');
-  const columnInput = document.getElementById('inputHeight');
-  const colorPicker = document.getElementById('colorPicker');
 
   const rows = parseInt(rowInput.value);
   const columns = parseInt(columnInput.value);
 
-  // Clear any existing cells
-  while (table.firstChild) {
-    table.removeChild(table.firstChild);
-  }
+  table.innerHTML = "";
 
-  // Create a <tr> for every row
   for (let r = 0; r < rows; ++r) {
     const rowElement = document.createElement('tr');
     table.appendChild(rowElement);
-    // within each row create a <td> for every column
+
     for (let c = 0; c < columns; ++c) {
       const dataElement = document.createElement('td');
       // handle click, set color
       function makeColor() {
         // get a new color value on each click
         // to support multi-color drawings
-        const colorPicker = document.getElementById('colorPicker');
         const color = colorPicker.value;
         dataElement.style.backgroundColor = color;
       }
